2b5b2770b0b9bfb4ea27372b4a60685ec6a3946f
Hellen61291 <Hellen61291@GB-LD1LPW10G010.mastek.com>
2020-02-07 00:50:04 +0000

    InventoryApp Master

---
+++ InventoryApp/src/test/java/com/mastek/InventoryApp/InventoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastek.InventoryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
